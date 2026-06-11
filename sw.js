a7577b31378a6f8fbb882c3ef926b787d57fed59
wahyuar0709-web <wahyuar0709@gmail.com>
2026-06-11 13:45:06 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'rdi-stok-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './scanner.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Jangan cache request ke GAS
+  if (e.request.url.includes('script.google.com')) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
